45790b9088af3a3252fe6d0c3d4b0b09fb3d535f
Luis Mayta <luis.mayta@gmail.com>
2020-07-07 00:29:17 -0500

    usando Local Storage

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -8,15 +8,24 @@ export class DeseosService {
   listas:Lista[]=[];
 
   constructor() {
-    const lista1 = new Lista('Recolectar Piedras del infinito');
-    const lista2 = new Lista('Heroes a desaparecer');
-    this.listas.push(lista1,lista2);
+    this.cargarStorage();
+    // const lista1 = new Lista('Recolectar Piedras del infinito');
+    // const lista2 = new Lista('Heroes a desaparecer');
+    // this.listas.push(lista1,lista2);
    }
    crearLista(titulo:string){
      const nuevaLista = new Lista(titulo);
      this.listas.push(nuevaLista);
-     const texto ='texto';
-     texto.length
-
+      this.guardarStorage();
+   }
+   guardarStorage(){
+     localStorage.setItem('data',JSON.stringify(this.listas));
+   }
+   cargarStorage(){
+     if (localStorage.getItem('data')) {
+       this.listas  = JSON.parse(localStorage.getItem('data'));
+     }else{
+       this.listas = [];
+     }
    }
 }
